c0a10bc70d64c7d2c2fcea2b32fd651100b4fb10
王一扬 <newwangyiyang@163.com>
2018-10-14 16:27:34 +0800

    mybatis的代码生成工具dao,domain,controller,service

---
+++ src/main/java/com/goboo/RenrenApplication.java
@@ -0,0 +1,14 @@
+package com.goboo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.goboo.dao")
+public class RenrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenrenApplication.class, args);
+	}
+}
